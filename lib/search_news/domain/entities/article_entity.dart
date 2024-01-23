1bf2b12697c9b58425a6094bbbeb7476d0ad904c
ahmeddo7a <ahmedholdo@gmail.com>
2024-01-23 11:53:09 +0200

    Ui-Part

---
+++ lib/search_news/domain/entities/article_entity.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class ArticleEntity extends Equatable{
+  final String title;
+  final String imageUrl;
+
+  const ArticleEntity({required this.title,required this.imageUrl});
+
+  @override
+  List<Object?> get props => [title,imageUrl];
+}
---
+++ lib/search_news/presentation/widgets/article_widget.dart
@@ -0,0 +1,30 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:newsapitest/search_news/domain/entities/article_entity.dart';
+
+class ArticleWidget extends StatelessWidget {
+  final ArticleEntity article;
+  const ArticleWidget({super.key,required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    final myWidth = MediaQuery.sizeOf(context).width;
+    final myHeight = MediaQuery.sizeOf(context).height;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment:  CrossAxisAlignment.center,
+      children: [
+        CachedNetworkImage(
+            width: myWidth*0.3,
+            height: myHeight*0.1,
+            imageUrl: article.imageUrl,
+            fit: BoxFit.contain,
+
+        ),
+        SizedBox(width: myWidth*0.02,),
+        Expanded(child: Text(article.title,overflow: TextOverflow.ellipsis,))
+      ],
+    );
+  }
+}
